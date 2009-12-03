8836f52eb990f6036b5779faf3b26bd9d00e156e
Jake Mitchell <jmitchell@parallax.(none)>
2009-12-02 20:26:16 -0500

    created generic script for scraping unit test names.  depends on proper configuration of the constants.rb file.

---
+++ harness/unit_test_scraper/sample_tests/my_test.py
@@ -0,0 +1,13 @@
+import aslksj
+import lksadkh
+
+class SomeObject:
+    def _a_b_c_(x=1,y=2):
+        return y
+
+    def _1_2_3_(x=[1,2,3],y='test',z="test2"):
+	return z
+
+    def test_easy():
+	return 1
+
---
+++ harness/unit_test_scraper/test.py
@@ -0,0 +1,8 @@
+def test1(a,b,c):
+    print "x"
+
+def test2():
+    return 1+1
+
+def test3_DifferentChars(a,b,c,d,e,f):
+    print "hello"
