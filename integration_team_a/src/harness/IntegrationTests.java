dea08ad222376e398c980504bda0140c056abd93
unknown <Josh@.(none)>
2009-12-17 02:47:21 -0500

    fuck this again 2

--- integration_team_a/src/harness/IntegrationTests.java
+++ integration_team_a/src/harness/IntegrationTests.java
@@ -9,7 +9,14 @@ public class IntegrationTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Unit test suite for edu.cs320.project");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(LoginUserIntegrationTest.class);
+		suite.addTestSuite(AllergyIntegrationTest.class);
+		suite.addTestSuite(CreateUserIntegrationTest.class);
+		suite.addTestSuite(DateWrapperIntegrationTest.class);
+		suite.addTestSuite(DisplayControllerIntegrationTest.class);
+		suite.addTestSuite(DrugIntegrationTest.class);
+		suite.addTestSuite(LoginIntegrationTest.class);
+		suite.addTestSuite(QuestionSetIntegrationTest.class);
+		suite.addTestSuite(Search2IntegrationTest.class);
 		suite.addTestSuite(SearchIntegrationTest.class);
 		//$JUnit-END$
 		return suite;
