84e07f92f4f0ba4db2cd8e8183f979d5335c172c
Timothy Yen <tyen@student.umass.edu>
2009-12-17 02:20:37 -0500

    Changed zip folder

--- zip.sh
+++ zip.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git checkout-index -a -f --prefix=checkout_test/
-find ./checkout_test -name '.gitignore' | xargs rm
-zip -r test_harness.zip ./checkout_test 
-rm -rf ./checkout_test
+git checkout-index -a -f --prefix=zip_temp/
+find ./zip_temp -name '.gitignore' | xargs rm
+zip -r test_harness.zip ./zip_temp 
+rm -rf ./zip_temp
